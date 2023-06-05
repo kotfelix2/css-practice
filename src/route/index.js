0c5f11949ce2bec5ee2682bd348ea0c1e5273d32
Oleksandr Belinskyi <oleksandrbelinskyi@Oleksandrs-MacBook-Pro.local>
2023-06-05 19:55:43 +0300

    home slack

--- src/route/index.js
+++ src/route/index.js
@@ -25,7 +25,9 @@ router.get('/slack', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('slack', {})
+  res.render('slack', {
+    layout: null,
+  })
   //                  ↑↑ сюди вводимо JSON дані
 })
 
